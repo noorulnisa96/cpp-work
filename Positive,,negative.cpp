b3f7afcd855b2a0690a647bb63f4efe1607069db
Noor ul nisa <141630403+noorulnisa96@users.noreply.github.com>
2023-08-07 15:50:43 +0500

    Add files via upload

---
+++ Positive,,negative.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter number:";
+	cin>>n;
+	
+	if(n>0)
+	{
+		cout<<"Number is positive";
+	}
+	else
+	{
+		cout<<"Number is negative";
+	}
+}

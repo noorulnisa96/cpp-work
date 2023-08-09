de2b9f7c07f0fcc5f5dfba1e9baea40a76df0cb1
Noor ul nisa <141630403+noorulnisa96@users.noreply.github.com>
2023-08-09 18:20:36 +0500

    Add files via upload

---
+++ Percentage of all subj.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int A,B,C,D,E,F,G,H,percent,total,obtained,p;
+	cout<<"Enter your all subj Marks:"<<endl;
+	cin>>A>>B>>C>>D>>E>>F>>G>>H;
+	
+	obtained=A+B+C+D+E+F+G+H;
+	cout<<"Enter Total Marks:";
+	cin>>total;
+	
+	cout<<"You obtained "<<obtained<<" marks."<<endl;
+	
+	p=obtained*100;
+	percent=p/total;
+	 cout<<"You have "<<percent<<"%."<<endl;
+	 
+	
+}
